68cb922dc20dd260d9d8561201fa2ed6f5d1bd9d
Alexandre Knijn <aknijn1@lsu.edu>
2017-03-08 08:09:42 -0600

    Implement basic kinematic movement using InputManager
    added horizontal and vertical movement using the InputManager

---
+++ Odyssey/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    Vector3 playerPos;
+    
+
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+        playerPos = gameObject.GetComponent<Transform>().position;
+        
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            gameObject.GetComponent<Transform>().position = new Vector3(playerPos.x + (Input.GetAxis("Horizontal") / 2), playerPos.y + (Input.GetAxis("Vertical")/2), playerPos.z);
+        }
+    }
+}

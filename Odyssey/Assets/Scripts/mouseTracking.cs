0593e3ce484f6a3d7f8ef55971eb302062dd812f
Alexandre Knijn <aknijn1@lsu.edu>
2017-03-08 10:50:19 -0600

    worked on reticle tracking
    worked on reticle training not working at all yet.

---
+++ Odyssey/Assets/Scripts/mouseTracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseTracking : MonoBehaviour {
+    Vector2 mousePos;
+
+	void Start ()
+    {
+       
+    }
+	
+	void Update ()
+    {
+        mousePos = Input.mousePosition;
+        GameObject.Find("reticle").GetComponent<Transform>().position = mousePos;
+	}
+}

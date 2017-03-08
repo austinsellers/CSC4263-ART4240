81898f2fb601624845a79d4a85322335df532771
Blake-Allen <blakeallen@Austins-MBP.wlan.lsu.edu>
2017-03-08 13:05:08 -0600

    Added Player Controller Script
    PlayerController script moves the player based on ridgedbody and is
    scaleable on the speed var

---
+++ Odyssey/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = .05f;
+	Rigidbody2D rigidBody;
+	Vector2 currentPos;
+	Vector2 initPos;
+	void Start () 
+	{
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+		initPos = gameObject.transform.position;
+
+	}
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			currentPos.x -= speed;
+			//this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			currentPos.x += speed;
+			//this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			currentPos.y+=speed;
+			//this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			currentPos.y-=speed;
+			//this.transform.position = position;
+		}
+		rigidBody.MovePosition (currentPos);
+	}
+}

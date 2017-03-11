43ff4c934c41036af501012d6ef73df706a28ca0
austinsellers <austinsellers@live.com>
2017-03-11 12:00:33 -0600

    Adds temp floor w/ grass prefab and Camera Follow script. Fixes script formatting and rendering layers

---
+++ Odyssey/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+	private GameObject player;
+	Vector3 offset;
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		offset = transform.position - player.transform.position;
+	}
+
+	void Update () 
+	{
+		transform.position = player.transform.position + offset;	
+	}
+}

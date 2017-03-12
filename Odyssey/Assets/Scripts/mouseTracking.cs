05de9db56b0c05e1b88d26463f506c465840f1de
austinsellers <austinsellers@live.com>
2017-03-12 16:11:15 -0500

    Fix mouse tracking and change reticle to hardware cursor

--- Odyssey/Assets/Scripts/mouseTracking.cs
+++ Odyssey/Assets/Scripts/mouseTracking.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mouseTracking : MonoBehaviour 
+public class MouseTracking : MonoBehaviour 
 {
+	Transform player;
     Vector2 mousePos;
+	private float moveSpeed = 1f;
 
-	void Start ()
-    {
-       
-    }
-	
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+	void Awake()
+	{
+		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+		Cursor.visible = true;
+	}
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void OnMouseExit()
+	{
+		Cursor.SetCursor (null, Vector2.zero, cursorMode);
+	}
+
 	void Update ()
     {
         mousePos = Input.mousePosition;
-        GameObject.Find("Reticle").GetComponent<Transform>().position = mousePos;
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		//.position = Vector2.Lerp (reticlePos.position, mousePos, moveSpeed);
+		/*player.rotation = Quaternion.Euler (0, 0, Mathf.Atan2 (mousePos.y - player.position.y, 
+			mousePos.x - player.position.x) * Mathf.Rad2Deg - 90);
+		Debug.Log (player.rotation);*/
 	}
 }

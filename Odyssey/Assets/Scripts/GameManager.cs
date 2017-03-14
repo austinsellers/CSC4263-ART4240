01a4e212950132e54d10bdc057c1a6dabea8bf57
austinsellers <austinsellers@live.com>
2017-03-13 23:33:11 -0500

    Add initial minimap and try to fix walk animation

---
+++ Odyssey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	public int playerHealth = 5;
+	public int playerExperience = 0;
+	public static GameManager instance = null;
+
+	void Awake () 
+	{
+		// Makes sure there is only 1 GameManager
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		// Doesn't destroy when reloading scene
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}

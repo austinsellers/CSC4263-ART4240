174935b2be7f57d1fedd05dbe26ff8ec67109b9a
Alexandre Knijn <aknijn1@lsu.edu>
2017-03-17 14:28:28 -0500

    finish bark & bite
    bark was given movement and both attacks were givien firing rates

--- Odyssey/Assets/Scripts/PlayerController.cs
+++ Odyssey/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
 
 	public float speed = 6f;
     public float barkSpd =1f;
+    bool ableToBark = true;
+    bool ableToBite = true;
+    float lastBark;
+    float lastBite;
+    float barkDelay = .5f;
+    float biteDelay = .5f;
 	float movement;
 
 	Color normalColor;
@@ -52,82 +58,109 @@ public class PlayerController : MonoBehaviour
 
 	void Update () 
 	{
-		// Can't move and animations won't play if paused
-		if (!GameManager.isPaused()) 
-		{
-			// Doesn't move until button is pressed
-			playerMove = false;
-			//projectedPos.Set(currentPos.x,currentPos.y);
-			if ((Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) && (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W))) {
-				direction = 7;
-				Move (0); // Move UP
-				Move (3); // Move LEFT
-				Rotate (30f);
-			} else if ((Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) && (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W))) {
-				direction = 4;
-				Move (0); // Move UP
-				Move (1); // Move RIGHT
-				Rotate (-30f);
-			} else if ((Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) && (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S))) {
-				direction = 5;
-				Move (2); // Move DOWN
-				Move (1); // Move RIGHT
-				Rotate (30f);
-			} else if ((Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) && (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S))) {
-				direction = 6;
-				Move (2); // Move DOWN
-				Move (3); // Move LEFT
-				Rotate (-30f);
-			} else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
-				// Flips the sprite to face Left
-				if (renderer.flipX)
-					renderer.flipX = false;
-				// If the player is facing Left
-				direction = 3;
-				Move (direction);
-				// Reset Rotation
-				Rotate (0f);
-			} else if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
-				// If the player is facing Up
-				direction = 0;
-				Move (direction);
-				// Reset Rotation
-				Rotate (0f);
-			} else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
-				// If the player is facing Down
-				direction = 2;
-				Move (direction);
-				// Reset Rotation
-				Rotate (0f);
-			} else if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
-				// Flips the sprite to face Right
-				if (!renderer.flipX)
-					renderer.flipX = true;
-				// If the player is facing Right
-				direction = 1;
-				Move (direction);
-				// Reset Rotation
-				Rotate (0f);
-			}
-			// If player is going left or right set that trigger (For Animation)
-			if (direction == 1 || direction == 3)
-				animator.SetBool ("playerLR", true);
-			else
-				animator.SetBool ("playerLR", false);
-			
-			// Sets if the player is moving (For Animation)
-			animator.SetBool ("playerMove", playerMove);
-			animator.SetInteger ("playerDir", direction);
-			if (Input.GetKey(KeyCode.J))
+        // Can't move and animations won't play if paused
+        if (!GameManager.isPaused())
+        {
+            // Doesn't move until button is pressed
+            playerMove = false;
+            //projectedPos.Set(currentPos.x,currentPos.y);
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
             {
-				BiteMake (direction);
-			}
-            if (Input.GetKey(KeyCode.K))
+                direction = 7;
+                Move(0); // Move UP
+                Move(3); // Move LEFT
+                Rotate(30f);
+            }
+            else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
             {
+                direction = 4;
+                Move(0); // Move UP
+                Move(1); // Move RIGHT
+                Rotate(-30f);
+            }
+            else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))
+            {
+                direction = 5;
+                Move(2); // Move DOWN
+                Move(1); // Move RIGHT
+                Rotate(30f);
+            }
+            else if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))
+            {
+                direction = 6;
+                Move(2); // Move DOWN
+                Move(3); // Move LEFT
+                Rotate(-30f);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                // Flips the sprite to face Left
+                if (renderer.flipX)
+                    renderer.flipX = false;
+                // If the player is facing Left
+                direction = 3;
+                Move(direction);
+                // Reset Rotation
+                Rotate(0f);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                // If the player is facing Up
+                direction = 0;
+                Move(direction);
+                // Reset Rotation
+                Rotate(0f);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                // If the player is facing Down
+                direction = 2;
+                Move(direction);
+                // Reset Rotation
+                Rotate(0f);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                // Flips the sprite to face Right
+                if (!renderer.flipX)
+                    renderer.flipX = true;
+                // If the player is facing Right
+                direction = 1;
+                Move(direction);
+                // Reset Rotation
+                Rotate(0f);
+            }
+            // If player is going left or right set that trigger (For Animation)
+            if (direction == 1 || direction == 3)
+                animator.SetBool("playerLR", true);
+            else
+                animator.SetBool("playerLR", false);
+
+            // Sets if the player is moving (For Animation)
+            animator.SetBool("playerMove", playerMove);
+            animator.SetInteger("playerDir", direction);
+            if (Input.GetKey(KeyCode.J) && ableToBite)
+            { 
+                ableToBite = false;
+                BiteMake(direction);
+                lastBite = Time.time;
+            }
+            if (Input.GetKey(KeyCode.K) && ableToBark)
+            {
+                ableToBark = false;
                 BarkMake(direction);
+                lastBark = Time.time;
+            }
+            if (ableToBark == false && (lastBark + barkDelay) < Time.time)
+            {
+                ableToBark = true;
+            }
+            if (ableToBite == false && (lastBite + biteDelay) < Time.time)
+            {
+                ableToBite = true;
             }
         }
-	}
+    }
 
 	void Rotate(float angle) 
 	{
@@ -170,7 +203,6 @@ public class PlayerController : MonoBehaviour
         {
             bite = (GameObject)Instantiate(Bite, new Vector3(currentPos.x + 2 * Mathf.Sin(((dir - 4) * (Mathf.PI / 2f)) + (Mathf.PI / 4)), currentPos.y + 2 * Mathf.Cos(((dir - 4) * (Mathf.PI / 2f)) + (Mathf.PI / 4f)), -5f), new Quaternion(0f, 0f, (dir * -Mathf.PI / 6), 0f));
         }
-        Debug.Log(dir);
     }
 
     void BarkMake(int dir)
